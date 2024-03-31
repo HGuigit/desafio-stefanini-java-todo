da26130e5598df99d84b5e9c6bdec7d3f81bd6c3
Guilherme de Souza (hguigit) <guilherme.silva@technosoftware.com.br>
2024-03-31 16:48:08 -0300

    fix: Arrumando ambientes docker para adaptação com AWS

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,9 @@ set -e
 
 JAR_FILE=$(find . -name 'todolist-*.jar' | sort -V | tail -n 1)
 
-java -jar "$JAR_FILE" --spring.profiles.active="$SPRING_PROFILE"
+if [ "$SPRING_PROFILE" = "local-completo" ]; then
+    dockerize -wait tcp://sql_server:1433 -timeout 240s
+    java -jar "$JAR_FILE" --spring.profiles.active="$SPRING_PROFILE"
+else
+    java -jar "$JAR_FILE" --spring.profiles.active="$SPRING_PROFILE"
+fi
